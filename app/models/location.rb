f1bc85bda95a473be771d24a785cbd6cca578da7
Trish Griego <pmgriego@gmail.com>
2017-01-11 12:56:58 -0800

    Succesfully used Rails geocode gem. For finding coordinates.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ApplicationRecord
+  # Looks like that code below is depricated. In favor of strong parameters.
+  # attr_accessible :address, :latitude, :longitude
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 end
